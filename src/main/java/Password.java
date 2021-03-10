11d995d526038a7c5e358431a8a8850b142de806
Norbert Heinrich <norbert.heinrich@stud.fh-campuswien.ac.at>
2021-03-10 13:40:33 +0100

    First tests running.

---
+++ src/main/java/Password.java
@@ -0,0 +1,23 @@
+public class Password {
+    String pw;
+
+    public Password(String pw){
+        this.pw = pw;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public Boolean checkLength() {
+        if(this.pw.length() >= 8 && this.pw.length() <= 25) {
+            return true;
+        }
+        return false;
+    }
+}
+
